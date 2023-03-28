a1f27f73a945b176119258e076ea44894ce2d0cf
Andy Short <automation.andy101@gmail.com>
2023-03-28 12:37:56 +0700

    App copied from my otehr GitHub account

---
+++ src/App1.js
@@ -0,0 +1,45 @@
+import { Component } from 'react';
+
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: {firstName: 'Andy', lastName: 'Short'},
+      company: 'ZTM'
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hi {this.state.name.firstName} {this.state.name.lastName}, I work at {this.state.company}
+          </p>
+          <button 
+            onClick={() => {
+              this.setState(
+                () => {
+                  return { 
+                    name: {firstName: 'Bill', lastName: 'Armstrong'} 
+                  };
+                }, () => {
+                  console.log(this.state)
+                }
+              );
+            }}
+          >
+            Change name
+          </button>
+        </header>
+      </div>
+    );
+  }
+}
+  
+  
+
+export default App;
